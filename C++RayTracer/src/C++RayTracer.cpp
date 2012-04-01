ed6a4970f77112ce17a1efdf5882a21a00e1715d
Jamie Davies <jd9993@bristol.ac.uk>
2012-04-01 12:51:14 +0100

    Added main file for ray tracer

---
+++ C++RayTracer/src/C++RayTracer.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : C++RayTracer.cpp
+// Author      : James Davies
+// Version     : 1
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Ray Tracer" << endl;
+	return 0;
+}
