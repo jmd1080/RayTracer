9d345693e92ec2f40a01c3d548ae97c53c798dce
Jamie Davies <jd9993@bristol.ac.uk>
2012-04-01 13:20:38 +0100

    Added opencv for image creation

--- C++RayTracer/src/C++RayTracer.cpp
+++ C++RayTracer/src/C++RayTracer.cpp
@@ -6,6 +6,11 @@
 //============================================================================
 
 #include <iostream>
+#include "cstdlib"
+#include "cmath"
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
 using namespace std;
 
 int main() {
